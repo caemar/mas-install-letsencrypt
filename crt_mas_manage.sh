3f8d11540a3c2d23649ecb67bf7fecf2ed21fda6
Martin Caesar <caesar@de.ibm.com>
2024-03-07 08:50:25 +0100

    Update crt_mas_manage.sh

--- crt_mas_manage.sh
+++ crt_mas_manage.sh
@@ -12,7 +12,7 @@ echo
 
 instance=$(echo $namespace | cut -d"-" -f2)
 
-workspace=$(oc get pod -n $namespace \
+workspace=$(oc get ManageWorkspace -n $namespace \
             -o jsonpath='{ .items[*].metadata.labels.mas\.ibm\.com/workspaceId }' \
             | awk '{ print $1 }')
 
@@ -43,43 +43,36 @@ openssl x509 -noout -issuer -subject -enddate -ext subjectAltName
 appsdomain=$(oc get ingresses.config/cluster -o jsonpath='{ .spec.domain }')
 
 for certname in \
-$workspace $workspace-all $workspace-cron $workspace-mea $workspace-rpt $workspace-ui
+manage \
+$workspace.manage \
+$workspace-all.manage \
+$workspace-cron.manage \
+$workspace-mea.manage \
+$workspace-rpt.manage \
+$workspace-ui.manage
 do
+name=$(echo $certname | sed "s/\./-/g")
 
 cat << EOF | oc create -f -
 apiVersion: cert-manager.io/v1
 kind: Certificate
 metadata:
-  name: letsencrypt-$instance-$workspace-cert-public-81-$certname
+  name: letsencrypt-$name
   namespace: $namespace
 spec:
-  secretName: $instance-$workspace-cert-public-81-$certname
+  secretName: letsencrypt-$name
   issuerRef:
     name: letsencrypt
     kind: ClusterIssuer
   dnsNames:
     - $instance.$appsdomain
-    - $certname.manage.$instance.$appsdomain
+    - $certname.$instance.$appsdomain
 EOF
+
+oc wait --for jsonpath={.status.conditions[0].status}=True \
+cert/letsencrypt-$name -n $namespace
 
 done
-
-cat << EOF | oc create -f -
-apiVersion: cert-manager.io/v1
-kind: Certificate
-metadata:
-  name: letsencrypt-$instance-$workspace-cert-public-81-$workspace-manage
-  namespace: $namespace
-spec:
-  secretName: $instance-$workspace-cert-public-81-$workspace-manage
-  issuerRef:
-    name: letsencrypt
-    kind: ClusterIssuer
-  dnsNames:
-    - $instance.$appsdomain
-    - manage.$instance.$appsdomain
-    - maxinst.manage.$instance.$appsdomain
-EOF
 
 cat << EOF | oc create -f -
 apiVersion: cert-manager.io/v1
@@ -116,3 +109,12 @@ echo oc get cert letsencrypt-$instance-$workspace-cert-public-81 -n $namespace
 oc set data secret/$instance-$workspace-cert-public-81 \
 -n $namespace \
 --from-file ca.crt=isrgrootx1.pem
+
+oc delete cert -n $namespace \
+letsencrypt-manage \
+letsencrypt-$workspace-manage \
+letsencrypt-$workspace-all-manage \
+letsencrypt-$workspace-cron-manage \
+letsencrypt-$workspace-mea-manage \
+letsencrypt-$workspace-rpt-manage \
+letsencrypt-$workspace-ui-manage
